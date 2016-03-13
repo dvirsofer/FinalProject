d30062d40311d22627d7be4b114f27eaf2a6183d
dvirs <dvir.sofer@walla.com>
2016-03-13 14:24:29 +0200

    add table of customers

--- public/js/search.js
+++ public/js/search.js
@@ -3,9 +3,9 @@
     $('#search_dropdown').on('change', searchDropdownEvent);
     $('#customers_dropdown').on('change', customerDropdownEvent);
     $('#form_by_passport').submit(formByPassportSubmit);
-    $('#workers').DataTable({
+    /*$('#workers').DataTable({
         responsive: true
-    });
+    });*/
     $('#contacts').DataTable({
         responsive: true
     });
