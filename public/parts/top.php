7cee766fb81f64f94faaf54a098a7247aebddce0
dvirs <dvir.sofer@walla.com>
2016-03-14 14:07:38 +0200

    icon

--- public/parts/top.php
+++ public/parts/top.php
@@ -18,8 +18,8 @@
     <!-- Custom CSS -->
     <link href="/public/css/sb-admin-2.css" rel="stylesheet">
     <!-- Custom Fonts -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" />
     <link href="/public/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" />
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
