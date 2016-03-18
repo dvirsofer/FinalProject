982b3c6e7027ec8a3feb83ad6a8aa143f1d0bcc8
dvirs <dvir.sofer@walla.com>
2016-03-18 11:43:47 +0200

    .

--- Configure.php
+++ Configure.php
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 16/03/2016
- * Time: 20:29
- */
-
-//define("SERVER_NAME","http://localhost:8000/finalProject");
-
-=======
 <?php
 
 //define("SERVER_NAME","http://localhost:8000/finalProject");
 
->>>>>>> caef51f86359ac81fa5a1a440d2d7dc42d6dd372
 define("SERVER_NAME","http://52.25.230.58");
