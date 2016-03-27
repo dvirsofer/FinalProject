4e5b8aa85d2f4955c512b00b6f07c1987b44a568
igorblohov <blohov@gmail.com>
2016-03-28 01:32:15 +0300

    google webservice added

--- Configure.php
+++ Configure.php
@@ -1,5 +1,5 @@
 <?php
 
-define("SERVER_NAME","http://localhost:8000");
+//define("SERVER_NAME","http://localhost:8000");
 
-//define("SERVER_NAME","http://52.25.230.58");
+define("SERVER_NAME","http://52.25.230.58");
