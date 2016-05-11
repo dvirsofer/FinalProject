46995cf9e67a0586a82a75e1496896f46abde1bc
dvirs <dvir.sofer@walla.com>
2016-05-11 12:44:51 +0300

    .

--- controller/MailController.php
+++ controller/MailController.php
@@ -67,6 +67,7 @@ class MailController
         $body .= "ליעד " . $target . "\n";
         $body .= "מהתאריך " . $dereliction_date . " " . "עד לתאריך " . $arrival_date;
         $body = wordwrap($body, 70, "\r\n");
+
         $headers = 'From: '.$from."\r\n".
             'Reply-To: '.$from."\r\n" .
             'X-Mailer: PHP/' . phpversion();
@@ -118,17 +119,17 @@ class MailController
         // $this->mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
         $this->mail->SMTPAuth = true; // authentication enabled
         $this->mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-        //$this->mail->Host = "smtp.gmail.com";
         $this->mail->Host = "smtp-pulse.com";
         $this->mail->Port = 465; // or 587
         $this->mail->Username = "mbtmProject@gmail.com";  // SMTP username
-        //$this->mail->Password = "mbtm1234";
-        $this->mail->Password = "F5sGK4AK9mPoY"; // SMTP password
+        $this->mail->Password = "mbtm1234"; // SMTP password
+
+
 
         //$this->mail->From = $from;
         //$this->mail->FromName =$from;
-        $this->mail->SetFrom("mbtmProject@gmail.com", $from);
-        $this->mail->AddAddress("mbtmProject@gmail.com");
+        $this->mail->SetFrom('mbtmProject@gmail.com', 'b@gmail.com');
+        $this->mail->AddAddress('mbtmProject@gmail.com');
 
         // $this->mail->AddReplyTo("info@example.com", "Information");
 
