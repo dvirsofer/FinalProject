0f5bc7d1849e04164a87d93ff0950e4371e7097f
igorblohov <blohov@gmail.com>
2016-05-15 00:53:36 +0300

    order by settelment type

--- public/webservice/workers_webservice.php
+++ public/webservice/workers_webservice.php
@@ -19,69 +19,85 @@ $db->checkConnection();
 if(isset($_POST['new_area_field']) && strlen($_POST['new_area_field']) >2)
 {
     update_customer_area_fields($_POST['new_area_field'],$customer_name_in_hebrew,$db);
+    $search_area = $_POST['new_area_field'];
 }
 
+elseif($_POST['workers_fields'] !='אחר')
+{
+    $search_area = $_POST['workers_fields'];
+}
 
-$sql ="SELECT
+if($_POST['userOptionsRadios']=='work_experience')
+{
+    search_worker_by_experience($db,$search_area);
+}
+
+else {
+    $sql = "SELECT
 settlement.latitude,
 settlement.longitude,
 settlement.settlement_name,
-settlement.settlement_type
+settlement.setelment_type
 FROM mbtm_workers.customer
  RIGHT join settlement on   customer.settlement_id  = settlement.id
- where responsible_id = ". $_SESSION['user_id'] . " and customer.customer_name  = ('".$customer_name_in_hebrew."') LIMIT 0, 1;";
+ where responsible_id = " . $_SESSION['user_id'] . " and customer.customer_name  = ('" . $customer_name_in_hebrew . "') LIMIT 0, 1;";
 
-$list1 = $db->sql_query($sql);
+    $list1 = $db->sql_query($sql);
+
+    if (is_null($list1[0]->setelment_type))
+        $settlement_type_sql = "settlement.setelment_type is null or settlement.setelment_type = 'moshav'";
+    else
+        $settlement_type_sql = "settlement.setelment_type = '" . $list1[0]->setelment_type . "'";
 
 
-$sql ="SELECT
+    $sql = "SELECT
 settlement.latitude,
 settlement.longitude,
 settlement.settlement_name,
 customer.customer_name,
-settlement.settlement_type,
+settlement.setelment_type,
 forgen_workes.ammount_of_workers
 FROM mbtm_workers.customer
  left join settlement on   customer.settlement_id  = settlement.id
  INNER JOIN (SELECT current_customer_id,count(current_customer_id) as ammount_of_workers FROM mbtm_workers.forgen_workes
- where responsible_id = ". $_SESSION['user_id'] . " group by current_customer_id) as forgen_workes on customer.id = current_customer_id
+ where responsible_id = " . $_SESSION['user_id'] . " group by current_customer_id) as forgen_workes on customer.id = current_customer_id
 
- where responsible_id = ". $_SESSION['user_id'] . " and settlement.settlement_type = '". $list1[0]->settlement_type . "'  and customer.customer_name  <> ('".$customer_name_in_hebrew."') GROUP BY customer.customer_name ORDER BY customer.customer_name ASC;";
-
-
-$list = $db->sql_query($sql);
+ where responsible_id = " . $_SESSION['user_id'] . "  and customer.customer_name  <> ('" . $customer_name_in_hebrew . "')  and  " . $settlement_type_sql . " GROUP BY customer.customer_name ORDER BY customer.customer_name ASC;";
 
 
-$dis_array=array();
-for($i=0  ;$i<count($list);$i++) {
+    $list = $db->sql_query($sql);
 
-    $distance=getDistance($list1[0]->latitude,$list1[0]->longitude,$list[$i]->latitude,$list[$i]->longitude);
-   // var_dump($list[$i]);
-    $dis_array[$i]=array(
-    customer_name =>   $list[$i]->customer_name,
-    settlement_name => $list[$i]->settlement_name,
-    distance => $distance,
-    ammount_of_workers=> $list[$i]->ammount_of_workers
-);
 
+    $dis_array = array();
+    for ($i = 0; $i < count($list); $i++) {
+
+        $distance = getDistance($list1[0]->latitude, $list1[0]->longitude, $list[$i]->latitude, $list[$i]->longitude);
+        // var_dump($list[$i]);
+        $dis_array[$i] = array(
+            customer_name => $list[$i]->customer_name,
+            settlement_name => $list[$i]->settlement_name,
+            distance => $distance,
+            ammount_of_workers => $list[$i]->ammount_of_workers
+        );
+
+    }
+    usort($dis_array, "sort_by_dist");
+
+    $dis_array = array_slice($dis_array, 0, 10);
+
+
+    echo include('../parts/free_workers_by_distance.html');
+    die();
 }
-usort($dis_array, "sort_by_dist");
-
-$dis_array = array_slice($dis_array,0,10);
-
-
-
-echo include('../parts/free_workers.html');die();
-
 function sort_by_dist($distance1,$distance2)
 {
     return $distance1['distance']>$distance2['distance'];
 }
 
- function getDistance($lat1, $lon1, $lat2, $lon2) {
+function getDistance($lat1, $lon1, $lat2, $lon2) {
     //echo " lat1: ".$lat1." lon1: ". $lon1." lat2: ". $lat2." lon2: ". $lon2;
-     if($lat1==$lat2 && $lon1 == $lon2)
-         return 0;
+    if($lat1==$lat2 && $lon1 == $lon2)
+        return 0;
     $theta = $lon1 - $lon2;
     $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
     $dist = acos($dist);
@@ -109,6 +125,23 @@ function update_customer_area_fields($fields,$customer_name_in_hebrew,$db)
         if(empty($customerFields))
             $db->insert('customer_fields',['field_id'=>$list[0]->id,'customer_id'=>$customerId[0]->id]);
     }
+
+
+}
+
+function search_worker_by_experience($db,$area)
+{
+
+   // $field = $db->getTableData("activity_fields",[activity_name=>$area],null,true);
+    $subFieldsArr=explode (" ",$area);
+    $likeSql="";
+    foreach($subFieldsArr as $value)
+    {
+        $likeSql = " like %". $value . "% or";
+    }
+    $likeSql= rtrim($likeSql,"or");
+
+    $fieldArr =$db->sql_query("select id from activity_fields WHERE activity_name " . $likeSql);
 
 
 }
