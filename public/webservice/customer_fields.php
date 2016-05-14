1834c44cf90dfebb6cc4082b4f859629ced9cbd0
igorblohov <blohov@gmail.com>
2016-05-14 18:50:14 +0300

    distance or experience

--- public/webservice/customer_fields.php
+++ public/webservice/customer_fields.php
@@ -13,12 +13,17 @@ $customer_name_in_hebrew = $_POST['customer_name'];
 $db = DB::getInstance();
 $db->checkConnection();
 
+
+
+
+
+
 $sql ="select activity_name from activity_fields right join
 
 (select customer_fields.field_id from customer_fields WHERE customer_id =
 	(
 		SELECT id from
-		customer where customer_name  = ('".$customer_name_in_hebrew."') LIMIT 0, 1
+		customer where customer_name  = ('".$customer_name_in_hebrew."') and responsible_id = ". $_SESSION['user_id'] . "  LIMIT 0, 1
     )
  ) as customer_fields on customer_fields.field_id = activity_fields.id";
 
