6f84ad7a1aa3d0443b5ab8fb4f397fdedcebae69
igorblohov <blohov@gmail.com>
2016-05-14 15:44:11 +0300

    add auto complit field css added

--- public/js/shiftOrganizer.js
+++ public/js/shiftOrganizer.js
@@ -35,11 +35,11 @@ function set_activity_item(item) {
 
 $(function() {
     $("#settlement_list_id").mouseout(function () {
-        $("#settlement_list_id").css("display", "none");
+      //  $("#settlement_list_id").css("display", "none");
     });
 
     $("#new_area_field_list").mouseout(function () {
-        $("#new_area_field_list").css("display", "none");
+        //$("#new_area_field_list").css("display", "none");
     });
 
 })
