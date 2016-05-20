327f2062967f189ac733fefde2717d6ca173e906
igorblohov <blohov@gmail.com>
2016-05-20 17:04:52 +0300

    integrat worker system to main

--- view/ShiftOrganizerView.php
+++ view/ShiftOrganizerView.php
@@ -29,7 +29,7 @@ class ShiftOrganizerView
     public function showMainPage()
     {
         $_SESSION['user_id'] = unserialize($_SESSION['user'])[0]->id;
-        $user_type =  unserialize($_SESSION['user'])[0]->user_type;
+        $user_type =  unserialize($_SESSION['user'])[0]->type_id;
         $workersCount=  $this->shiftOrganizerModel->getWorkerAmount($_SESSION['user_id']);
         $employeeCount=  $this->shiftOrganizerModel->getEmployeeAmount($_SESSION['user_id']);
         $citiesCount=  $this->shiftOrganizerModel->getCitiesAmount($_SESSION['user_id']);
